09e8b6baf28159fd41b67d0c9c14f1554acaee32
nsutanto <nicholas.sutanto@gmail.com>
2017-08-19 18:55:42 -0400

    work in progress

--- MemeMe/SentMemesTableViewController.swift
+++ MemeMe/SentMemesTableViewController.swift
@@ -17,33 +17,46 @@ class SentMemesTableViewController: UITableViewController {
 
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         memes = appDelegate.memes
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        tableView.reloadData()
     }
     
-    @IBAction func createMeme(_ sender: Any) {
-        performSegue(withIdentifier: "segueCreateMeme", sender: sender)
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //TODO: Implement to push the next story node.
+        //let nextStoryNode = storyNode.storyNodeForIndex(index: (indexPath as NSIndexPath).row)
+        //let controller = self.storyboard!.instantiateViewController(withIdentifier: "StoryNodeViewController") as! StoryNodeViewController
+        //controller.storyNode = nextStoryNode
+        //self.navigationController!.pushViewController(controller, animated: true)
+        
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        //if segue.identifier == "segueShowResult" {
-        //    let vc = segue.destination as! ResultViewController
-        //    vc.userChoice = getUserShape(sender as! UIButton)
-        //}
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memes.count
     }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let memeImage = memes[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableCustomCell") as! TableCustomCell
+        
+        
+        cell.memeImage!.image = memeImage.memedImage
+        cell.memeText!.text = "\(memeImage.topText) ... \(memeImage.bottomText)"
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        return cell
+        
+        
+        //cell.textLabel!.text = storyNode.promptForIndex((indexPath as NSIndexPath).row)
+        
+        
+        //return cell
+        
+        //let cell = self.tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        //let adventure = adventures[(indexPath as NSIndexPath).row]
+        //cell.textLabel!.text = adventure.credits.title
+        //cell.detailTextLabel!.text = adventure.credits.author
+        //let imageName = adventure.credits.imageName
+        //cell.imageView!.image = UIImage(named: imageName!)
     }
-    */
 
 }
